c25d7b265fcd19c07d077622f896a038ff600fe2
Andres Felipe Moreno Echavarria <andresf.moreno@comware.com.co>
2026-06-12 17:43:42 -0500

    Primer push con estructura base de proyecto y configuracion git ignore

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// ============================================================
+// SISTEMA DE INVENTARIO - Clase 1.1
+// Estado: Mensaje de bienvenida
+// ============================================================
+
+using System.Reflection;
+
+var assembly = Assembly.GetExecutingAssembly();
+var version = assembly.GetName().Version;
+
+
+Console.WriteLine("==========================================");
+Console.WriteLine("    SISTEMA DE GESTIÓN DE INVENTARIO      ");
+Console.WriteLine("==========================================");
+Console.WriteLine();
+Console.WriteLine($"Version:{version}");
+Console.WriteLine($"Versión: 1.0.0");
+Console.WriteLine($"Plataforma: {Environment.OSVersion}");
+Console.WriteLine($".NET Version: {Environment.Version}");
+Console.WriteLine();
+Console.WriteLine("Estructura del Proyecto");
+Console.WriteLine("Configuracion .csproj");
+Console.WriteLine("Carpeta source");
+Console.WriteLine();
+Console.WriteLine();
